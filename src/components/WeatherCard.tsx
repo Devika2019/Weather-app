9f54bf2b8fb8726dcab4c830e14793f9e6f5948f
@Devika2019 <devikaraj92@gmail.com>
2024-08-22 13:33:59 -0400

    Weathercard style update

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, Box } from '@mui/material';
+import { Card, CardContent, Typography, Box, Avatar } from '@mui/material';
 import { WeatherResponse } from '../features/weather/weatherInterfaces';
 
 interface WeatherCardProps {
@@ -7,29 +7,81 @@ interface WeatherCardProps {
 }
 
 const WeatherCard: React.FC<WeatherCardProps> = ({ weatherData }) => {
-  const { main,weather,wind,name } = weatherData;
+  const { main, weather, wind, name } = weatherData;
   const { temp, humidity } = main;
   const weatherDescription = weather[0]?.description || 'No description';
   const weatherIcon = weather[0]?.icon || '';
-  const {speed} = wind;
+  const { speed } = wind;
 
   return (
-    <Card sx={{ marginBottom: 2 }}>
+    <Card 
+      sx={{ 
+        width: 300, 
+        margin: '20px auto', 
+        borderRadius: 2, 
+        boxShadow: 3,
+        backgroundColor: '#fff'
+      }}
+    >
       <CardContent>
-        <Typography variant="h5">{name}</Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="h6">Temperature: {temp}°C</Typography>
+        <Typography 
+          variant="h5" 
+          sx={{ 
+            fontWeight: 'bold', 
+            color: '#0277bd', 
+            marginBottom: 1          
+          }}
+        >
+          {name}
+        </Typography>
+        <Box 
+          sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            marginBottom: 1,
+            justifyContent:'center'
+          }}
+        >
+          <Typography 
+            variant="h6" 
+            sx={{ 
+              color: '#333', 
+              marginRight: 1 
+            }}
+          >
+            {temp}°C
+          </Typography>
           {weatherIcon && (
-            <img
-              src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
-              alt=''
-              style={{ marginLeft: 10 }}
+            <Avatar 
+              src={`http://openweathermap.org/img/wn/${weatherIcon}.png`} 
+              alt={weatherDescription} 
+              sx={{ 
+                width: 50, 
+                height: 50, 
+                borderRadius: '50%', 
+                backgroundColor: '#e0f7fa' 
+              }} 
             />
           )}
         </Box>
-        <Typography>Humidity: {humidity}%</Typography>
-        <Typography>Wind Speed: {speed} m/s</Typography>
-        <Typography>Description: {weatherDescription}</Typography>
+        <Typography 
+          variant="body1" 
+          sx={{ color: '#555', marginBottom: 0.5 }}
+        >
+          Humidity: {humidity}%
+        </Typography>
+        <Typography 
+          variant="body1" 
+          sx={{ color: '#555', marginBottom: 0.5 }}
+        >
+          Wind Speed: {speed} m/s
+        </Typography>
+        <Typography 
+          variant="body1" 
+          sx={{ color: '#555' }}
+        >
+          Description: {weatherDescription}
+        </Typography>
       </CardContent>
     </Card>
   );
